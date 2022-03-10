c14e1a263d09b9bd93b6a61a6068206957823805
cortalabocha <gabriel.cortacans@estudiantes.utec.edu.uy>
2022-03-10 04:28:44 -0300

    versión 0

---
+++ Mongo/src/Prueba/Conneccion.java
@@ -0,0 +1,42 @@
+package Prueba;
+
+
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+ 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+ 
+import static java.util.Arrays.asList;
+import java.util.LinkedList;
+ 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Conneccion {
+private static MongoClient mongoClient;
+private static MongoDatabase mongodb;
+		
+	
+public static void main(String[] args) {        
+	
+  mongoClient = new MongoClient("localhost",27017);
+    //((MongoClient) mongodb).getDatabase("tos");}
+	mongodb = mongoClient.getDatabase("corta");
+	//mongodb.createCollection("Libros");
+	
+	Document libro =  new Document();
+	libro.append("titulo","El hombre que dormia");
+	libro.append("autor"," Malba Disco ");
+	
+	mongodb.getCollection("Libros").insertOne(libro);
+	
+}
+	
+}
